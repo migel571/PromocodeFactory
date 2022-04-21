fcdb3e37f94cd0e23d01157d3196410868c466b0
Mike <dydydwdw@mail.ru>
2022-04-21 18:56:08 +0300

    Pre migration

---
+++ PromocodeFactory.Domain/PromocodeManagement/Partner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromocodeFactory.Domain.PromocodeManagement
+{
+    public class Partner
+    {
+        public string Name { get; set; }    
+        public  int NumberIssuedPromoCode { get; set; } 
+        public  bool IsActive { get; set; }
+        public List<PartnerPromoCodeLimit> PartnerLimits { get; set; }
+        public List<PromoCode> PromoCodes { get; set; }
+    }
+
+    
+}
--- PromocodeFactory.Infrastructure/PromocodeContext.cs
+++ PromocodeFactory.Infrastructure/PromocodeContext.cs
@@ -17,14 +17,42 @@ namespace PromocodeFactory.Infrastructure
         public DbSet<Preference> Preferences { get; set; }
         public DbSet<PromoCode> PromoCodes { get; set; }
 
-
+        /// <summary>
+        /// Fluent API
+        /// hasone(many) - связь данной сущности к другой 
+        /// withone(many) - обратная связь 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Employee>().ToTable("Employee").Property(p => p.Email).HasMaxLength(10);
+            //Конфигурация сущности Employee
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+            modelBuilder.Entity<Employee>().Property(p => p.Email).HasMaxLength(20);
+            modelBuilder.Entity<Employee>().Property(p => p.FirstName).HasMaxLength(20);
+            modelBuilder.Entity<Employee>().Property(p => p.LastName).HasMaxLength(20);
+            modelBuilder.Entity<Employee>().HasOne(p=>p.Role);
+            
+            //Конфигурация сущности Role
             modelBuilder.Entity<Role>().ToTable("Role");
+            modelBuilder.Entity<Role>().Property(p => p.RoleName).HasMaxLength(15);
+
+            //Конфигурация сущности Customer
             modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Customer>().Property(p => p.FirstName).HasMaxLength(20);
+            modelBuilder.Entity<Customer>().Property(p => p.LastName).HasMaxLength(20);
+            modelBuilder.Entity<Customer>().Property(p => p.Email).HasMaxLength(20);
+            modelBuilder.Entity<Customer>().HasMany(p => p.Preferences).WithMany(p=>p.Customers);
+            modelBuilder.Entity<Customer>().HasOne(p => p.PromoCode).WithMany(p => p.Customers);
+
+            //Конфигурация сущности Preference
             modelBuilder.Entity<Preference>().ToTable("Preference");
+            modelBuilder.Entity<Preference>().Property(p => p.Name).HasMaxLength(100);
+
+            //Конфигурация сущности PromoCode
             modelBuilder.Entity<PromoCode>().ToTable("PromoCode");
+            modelBuilder.Entity<PromoCode>().Property(p=>p.Code).HasMaxLength(100);
+            modelBuilder.Entity<PromoCode>().Property(p => p.PartnerName).HasMaxLength(20);
+            modelBuilder.Entity<PromoCode>().HasOne(p=>p.Preference);
 
 
         }
